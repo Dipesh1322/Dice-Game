a2ec5cccdd5ee73fd8cbafcdf365015cb765bb1e
Dipesh Chaudhary <108289234+Dipesh1322@users.noreply.github.com>
2022-12-28 12:10:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-// // Dice 1
-// let randomNumber1 = Math.floor(Math.random()*6+1);
-// let randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
-// document.querySelector(".img1").setAttribute("src",randomDiceImage1);
+document.querySelector("h1").addEventListener("click",handleClick);
+    function handleClick(){
+            // Dice 1
+            let randomNumber1 = Math.floor(Math.random() * 6 + 1);
+            let randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
+            document.querySelector(".img1").setAttribute("src", randomDiceImage1);
 
 
-// // Dice 2
-// let randomNumber2 = Math.floor(Math.random()*6+1);
-// let randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
-// document.querySelector(".img2").setAttribute("src",randomDiceImage2);
+            // Dice 2
+            let randomNumber2 = Math.floor(Math.random() * 6 + 1);
+            let randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+            document.querySelector(".img2").setAttribute("src", randomDiceImage2);
 
 
-// if(randomNumber1===randomNumber2){
-// document.querySelector("h1").innerHTML="Arre ye to DRAW hogaya!";
-// document.querySelector("h1").style.fontSize = "6rem";
-// }
-// else if(randomNumber1>randomNumber2){
-//     document.querySelector("h1").innerHTML="🚩Dipesh Wins!";
-// }
-// else{
-//     document.querySelector("h1").innerHTML="Chirkut Wins!🚩";
-// }
-
+            if (randomNumber1 === randomNumber2) {
+                document.querySelector("h1").innerHTML = "Arre ye to DRAW hogaya!";
+                document.querySelector("h1").style.fontSize = "6rem";
+            }
+            else if (randomNumber1 > randomNumber2) {
+                document.querySelector("h1").innerHTML = "🚩Dipesh Wins!";
+            }
+            else {
+                document.querySelector("h1").innerHTML = "Chirkut Wins!🚩";
+            }
+    }
 
 document.addEventListener("keydown", function (event) {
     handlePress(event.key);
