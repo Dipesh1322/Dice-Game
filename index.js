3e8039369932458c65b3825345bc3270c5973c44
Dipesh Chaudhary <108289234+Dipesh1322@users.noreply.github.com>
2023-03-02 21:04:03 +0530

    Update animation changes in index.js
    Added toggle class list to both the images so as to implement animation on each click.

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-document.querySelector("h1").addEventListener("click",handleClick);
+document.querySelector("button").addEventListener("click",handleClick);
     function handleClick(){
             // Dice 1
             let randomNumber1 = Math.floor(Math.random() * 6 + 1);
             let randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
             document.querySelector(".img1").setAttribute("src", randomDiceImage1);
-
+            document.querySelector(".img1").classList.toggle("diceRoll1");
+            document.querySelector(".img1").classList.toggle("diceRoll2");
 
             // Dice 2
             let randomNumber2 = Math.floor(Math.random() * 6 + 1);
             let randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
             document.querySelector(".img2").setAttribute("src", randomDiceImage2);
+            document.querySelector(".img2").classList.toggle("diceRoll1");
+            document.querySelector(".img2").classList.toggle("diceRoll2");
 
 
             if (randomNumber1 === randomNumber2) {
-                document.querySelector("h1").innerHTML = "Arre ye to DRAW hogaya!";
-                document.querySelector("h1").style.fontSize = "6rem";
+                document.querySelector("h1").innerHTML = "Oops, It's a Draw!";
+                document.querySelector("h1").style.fontSize = "5rem";
             }
             else if (randomNumber1 > randomNumber2) {
-                document.querySelector("h1").innerHTML = "🚩Dipesh Wins!";
+                document.querySelector("h1").innerHTML = "I Win!🚩";
             }
             else {
-                document.querySelector("h1").innerHTML = "Chirkut Wins!🚩";
+                document.querySelector("h1").innerHTML = "You Win!🚩";
             }
     }
 
@@ -43,14 +46,14 @@ function handlePress(enter) {
 
 
             if (randomNumber1 === randomNumber2) {
-                document.querySelector("h1").innerHTML = "Arre ye to DRAW hogaya!";
-                document.querySelector("h1").style.fontSize = "6rem";
+                document.querySelector("h1").innerHTML = "Oops, It's a Draw!";
+                document.querySelector("h1").style.fontSize = "5rem";
             }
             else if (randomNumber1 > randomNumber2) {
-                document.querySelector("h1").innerHTML = "🚩Dipesh Wins!";
+                document.querySelector("h1").innerHTML = "🚩I Win!";
             }
             else {
-                document.querySelector("h1").innerHTML = "Chirkut Wins!🚩";
+                document.querySelector("h1").innerHTML = "You Win!🚩";
             }
             break;
         default:
